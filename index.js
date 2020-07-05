f032ea11437afabae94c3f319026ec9741b1e5d3
Slava Dodonov <slavadodonov19@gmail.com>
2020-07-05 17:29:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const https = require('https');
+const rp = require('request-promise');
 const querystring = require('querystring');
 
 /**
@@ -21,39 +21,19 @@ module.exports = function Weather(config) {
      * @param.id - Search by city id
      * @param.lon - One of the search parameters by geographical position. Longitude
      * @param.lat - One of the search parameters by geographical position. Latitude
+     *
+     * @returns {*|PromiseLike<any>|Promise<any>}
      */
 
     this.getWeather = function (filter) {
-        https.get(`https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(filter)}&appid=${config.api}&lang=${config.lang}&units=${config.units}`, (res) => {
-            const { statusCode } = res;
-            const contentType = res.headers['content-type'];
+        let options = {
+            uri: `https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(filter)}&appid=${config.api}&lang=${config.lang}&units=${config.units}`,
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
+        };
 
-            let error;
-            if (statusCode !== 200) {
-                error = new Error('Request Failed.\n' +
-                    `Status Code: ${statusCode}`);
-            } else if (!/^application\/json/.test(contentType)) {
-                error = new Error('Invalid content-type.\n' +
-                    `Expected application/json but received ${contentType}`);
-            }
-            if (error) {
-                // Consume response data to free up memory
-                res.resume();
-                return error.message;
-            }
-
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => { rawData += chunk; });
-            res.on('end', () => {
-                try {
-                    const parsedData = JSON.parse(rawData);
-                    if(config.full) parsedData
-                        else return { temp: parsedData.main.temp, id: parsedData.id, name: parsedData.name };
-                } catch (e) {
-                    return e.message;
-                }
-            });
-        })
+        return rp(options).then(data => data)
     }
 }
